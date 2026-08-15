72952e9cb072fe3c932672fd96d448f6af2d89e5
Jay Salvi <jaysalvi11@gmail.com>
2026-08-15 15:00:53 +0530

    Configure default Wolfram App ID

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 FEATHERLESS_API_KEY = os.getenv("FEATHERLESS_API_KEY", "rc_33ac9a53b845eeb21d5542378f2cd956c473b30169977600fccfa3a980c2b6bd")
 FEATHERLESS_API_URL = os.getenv("FEATHERLESS_API_URL", "https://api.featherless.ai/v1")
 
-WOLFRAM_APP_ID = os.getenv("WOLFRAM_APP_ID", "")
+WOLFRAM_APP_ID = os.getenv("WOLFRAM_APP_ID", "7RR3LRAYLR")
 WOLFRAM_API_URL = "http://api.wolframalpha.com/v1/result" # or Cloud API URL
 
 FIRECRAWL_API_KEY = os.getenv("FIRECRAWL_API_KEY", "fc-edd3db0c3f3849d099eca9ddc77248e8")
